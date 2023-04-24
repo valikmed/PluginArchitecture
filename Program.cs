bd8e689ab3a23db2f7d1bd10b94a47cd345daab0
Elisyum <medvediuk_m18@nuwm.edu.ua>
2023-04-25 02:55:12 +0300

    🤪

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+public static class Kata
+{
+    static void Main(string[] args) { }
+    public static string CountSheep(int n)
+    {
+        //string result = "";
+        //int cnt = 0;
+        //for (int i = 1; i <= n; i++)
+        //{
+        //    result += $"{i} sheep...";
+
+        //}
+
+        string result = "";
+        int cnt = 0;
+        while (n > cnt)
+        {
+            cnt++;
+            result += $"{cnt} sheep...";
+
+        }
+
+        return result;
+    }
+}
